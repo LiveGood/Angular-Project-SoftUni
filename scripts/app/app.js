31e8105572dfd483b4a301ec5da5eed16f0386ae
Deyan Dachev <FleshThps@gmail.com>
2014-12-28 17:55:41 +0200

    Added navigation bar.

--- scripts/app/app.js
+++ scripts/app/app.js
@@ -1,2 +1,13 @@
 var adApp = angular.module('adApp', ['ngRoute']);
 
+adApp.config(function($routeProvider) {
+   $routeProvider
+      .when('/home', {
+         templateUrl: 'partials/homePartial.html',
+         controller: 'HomeController'
+      })
+      .otherwise({ redirectTo: '/home' })
+});
+
+
+
